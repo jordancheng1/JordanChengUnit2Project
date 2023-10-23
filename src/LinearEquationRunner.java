d3066d5e18d7687527e172d757c0395daa37fdef
BT_3W2_04 <jordanc249@nystudents.net>
2023-10-23 09:31:27 -0400

    updated

--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -4,14 +4,21 @@ public class LinearEquationRunner {
     public static void main(String[]args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Welcome!");
-        System.out.print("Enter coordinate 1: ");
-        String coordinate1 = scan.nextLine();
-        
-
-        System.out.print("Enter coordinate 2: ");
-        String coordinate2 = scan.nextLine();
-
+        int x1 = -1;
+        int y1 = 5;
+        int x2 = 3;
+        int y2 = 10;
+        LinearEquation equation = new LinearEquation(x1, y1, x2, y2);
+        System.out.println("Equation: " + equation.equation());
+        System.out.println("Slope: " + equation.slope());
+        System.out.println("y-intercept: " + equation.yIntercept());
+        System.out.println("Distance: " + equation.distance());
+        System.out.println();
+        System.out.println("----- Line info -----");
+        System.out.println(equation.lineInfo());
+        System.out.println();
+        double testX = 4;
+        System.out.println("Coordinate for x: " + equation.coordinateForX(testX));
 
     }
 }
